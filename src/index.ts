0febd267d683717b33b6aca4aaee6caeecfaf659
JMiguelS0t0 <juanmiguelsoto4@gmail.com>
2023-10-28 17:28:01 -0500

    Index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import personaRouter from './routes/persona.router';
+import catalogoRouter from './routes/catalogo.router';
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-app.use('/api/persona', personaRouter);
+app.use('/api/catalogo', catalogoRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando puerto ${PORT}`)
