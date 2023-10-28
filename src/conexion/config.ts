988c578a8bd9068115a982aa50a9ec96b63f79f6
JMiguelS0t0 <juanmiguelsoto4@gmail.com>
2023-10-28 15:56:23 -0500

    API Base

---
+++ src/conexion/config.ts
@@ -0,0 +1,10 @@
+export const sqlConfig = {
+    user: 'sa',
+    password: '1234',
+    database: 'SWIII',
+    server: 'localhost',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true
+    }
+}
---
+++ src/routes/persona.router.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import * as PersonaController from '../controllers/persona.controller';
+import { Persona } from '../model/persona';
+const router = express.Router();
+
+router.get('/', (_, res) => {
+    PersonaController.GetPersonas()
+        .then((obj) => {
+            res.json(obj);
+        }).catch((e) => {
+            res.status(500).json(e);
+        });
+});
+
+router.post('/', (req: express.Request, res: express.Response) => {
+    PersonaController.PostPersona(req.body as Persona)
+        .then((f) => {
+            if (f)
+                res.status(201).send();
+            else
+                res.status(500).send()
+        }).catch((e) => {
+            res.status(500).json(e);
+        });
+});
+
+export default router;
